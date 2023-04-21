990dafe3edef2cc99f4e3033b1c28650f89d862c
Amish Tufail <amish.tufail2002@gmail.com>
2023-04-21 15:17:36 +0500

    Initial Commit

---
+++ Mapie/MapieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapieApp.swift
+//  Mapie
+//
+//  Created by Amish Tufail on 21/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
